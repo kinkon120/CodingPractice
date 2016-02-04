a69b2e711b8d680554214b9d5c9e0503b364102a
yicchang <yicchang@ebay.com>
2016-02-04 01:54:09 -0800

    Leetcode 136

---
+++ LeetCode/src/com/yichin/leetcode/P136_SingleNumber/Solution.java
@@ -0,0 +1,18 @@
+package com.yichin.leetcode.P136_SingleNumber;
+
+/*
+	Given an array of integers, every element appears twice except for one. Find that single one.
+	Note:
+	Your algorithm should have a linear runtime complexity. Could you implement it without using extra memory?
+*/
+
+public class Solution {
+	// XOR trick
+    public int singleNumber(int[] nums) {
+    	int x = 0;
+        for(int num : nums){
+        	x^=num;
+        }
+        return x;
+    }
+}
