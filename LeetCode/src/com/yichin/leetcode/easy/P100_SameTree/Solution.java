8b6baed779b6945cfd855a4a986b6f81ac648800
yicchang <yicchang@ebay.com>
2016-02-04 12:26:59 -0800

    LeetCode 100

---
+++ LeetCode/src/com/yichin/leetcode/easy/P100_SameTree/Solution.java
@@ -0,0 +1,26 @@
+package com.yichin.leetcode.easy.P100_SameTree;
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+
+/*
+Given two binary trees, write a function to check if they are equal or not.
+
+Two binary trees are considered equal if they are structurally identical and the nodes have the same value.
+ */
+public class Solution {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if(p==null && q==null)  return true;
+        if( (p==null && q!=null) || (p!=null && q==null))   return false;
+        
+        if(p.val != q.val)  return false;
+        // compare subtree
+        return isSameTree(p.left,q.left)&&isSameTree(p.right,q.right);
+    }
+}
