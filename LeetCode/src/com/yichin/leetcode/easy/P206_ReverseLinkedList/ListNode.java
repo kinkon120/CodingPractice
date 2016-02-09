7adb5717b9e2ae0e03618b6e971fa9f398316352
yicchang <yicchang@ebay.com>
2016-02-09 00:24:32 -0800

    LeetCode: 12/13/52/94/144/191/206/235/268

---
+++ LeetCode/src/com/yichin/leetcode/easy/P206_ReverseLinkedList/ListNode.java
@@ -0,0 +1,7 @@
+package com.yichin.leetcode.easy.P206_ReverseLinkedList;
+
+public class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x) { val = x; }
+}
---
+++ LeetCode/src/com/yichin/leetcode/medium/P144_BinaryTreePreorderTraversal/TreeNode.java
@@ -0,0 +1,8 @@
+package com.yichin.leetcode.medium.P144_BinaryTreePreorderTraversal;
+
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int x) { val = x; }
+}
